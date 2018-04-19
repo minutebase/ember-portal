d4d77ce248b6472152c77d1e1121ce749616e4e2
Richard Livsey <richard@livsey.org>
2018-04-19 12:57:18 +0100

    Use qunit-dom in tests & modernize

--- tests/acceptance/portal-test.js
+++ tests/acceptance/portal-test.js
@@ -1,66 +1,57 @@
-import { findAll, visit } from '@ember/test-helpers';
-import { run } from '@ember/runloop';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
-import startApp from '../helpers/start-app';
-
-var application;
 
 module('Acceptance | portal', function(hooks) {
-  hooks.beforeEach(function() {
-    application = startApp();
-  });
-
-  hooks.afterEach(function() {
-    run(application, 'destroy');
-  });
+  setupApplicationTest(hooks);
 
   test('visiting /', async function(assert) {
     await visit('/');
 
-    assert.equal(findAll(".header").length, 1, 'Has 1 header');
-    assert.equal(findAll(".footer").length, 1, 'Has 1 footer');
+    assert.dom(".header").exists({ count: 1 }, 'Has 1 header');
+    assert.dom(".footer").exists({ count: 1 }, 'Has 1 footer');
 
-    assert.equal(findAll("#examples-header").length, 1, 'Shows the example header');
-    assert.equal(findAll("#examples-footer").length, 1, 'Shows the example footer');
+    assert.dom("#examples-header").exists({ count: 1 }, 'Shows the example header');
+    assert.dom("#examples-footer").exists({ count: 1 }, 'Shows the example footer');
   });
 
   test('visiting /foo', async function(assert) {
     await visit('/foo');
 
-    assert.equal(findAll(".header").length, 1, 'Has 1 header');
-    assert.equal(findAll(".footer").length, 1, 'Has 1 footer');
+    assert.dom(".header").exists({ count: 1 }, 'Has 1 header');
+    assert.dom(".footer").exists({ count: 1 }, 'Has 1 footer');
 
-    assert.equal(findAll("#foo-header").length, 1, 'Shows the foo header');
-    assert.equal(findAll("#examples-footer").length, 1, 'Shows the example footer');
+    assert.dom("#foo-header").exists({ count: 1 }, 'Shows the foo header');
+    assert.dom("#examples-footer").exists({ count: 1 }, 'Shows the example footer');
   });
 
 
   test('visiting /bar', async function(assert) {
     await visit('/bar');
 
-    assert.equal(findAll(".header").length, 1, 'Has 1 header');
-    assert.equal(findAll(".footer").length, 1, 'Has 1 footer');
+    assert.dom(".header").exists({ count: 1 }, 'Has 1 header');
+    assert.dom(".footer").exists({ count: 1 }, 'Has 1 footer');
 
-    assert.equal(findAll("#examples-header").length, 1, 'Shows the example header');
-    assert.equal(findAll("#bar-footer").length, 1, 'Shows the bar footer');
+    assert.dom("#examples-header").exists({ count: 1 }, 'Shows the example header');
+    assert.dom("#bar-footer").exists({ count: 1 }, 'Shows the bar footer');
   });
 
   test('visiting /bar/baz', async function(assert) {
     await visit('/bar/baz');
 
-    assert.equal(findAll(".header").length, 1, 'Has 1 header');
-    assert.equal(findAll(".footer").length, 1, 'Has 1 footer');
+    assert.dom(".header").exists({ count: 1 }, 'Has 1 header');
+    assert.dom(".footer").exists({ count: 1 }, 'Has 1 footer');
 
-    assert.equal(findAll("#baz-header").length, 1, 'Shows the baz header');
-    assert.equal(findAll("#baz-footer").length, 1, 'Shows the baz footer');
+    assert.dom("#baz-header").exists({ count: 1 }, 'Shows the baz header');
+    assert.dom("#baz-footer").exists({ count: 1 }, 'Shows the baz footer');
   });
 
 
   test('can give portals classes', async function(assert) {
     await visit('/');
 
-    assert.equal(findAll("#ember-portal--header.header-portal").length, 1, 'header has a custom class');
-    assert.equal(findAll("#ember-portal--footer.footer-portal").length, 1, 'footer has a custom class');
+    assert.dom("#ember-portal--header.header-portal").exists({ count: 1 }, 'header has a custom class');
+    assert.dom("#ember-portal--footer.footer-portal").exists({ count: 1 }, 'footer has a custom class');
   });
 
 });
